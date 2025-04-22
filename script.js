555aec2ee1b8e14cbf464eb174fa0eb2ba698fec
Kerly Sarrias <kerlysarrias011@gmail.com>
2025-04-22 15:38:45 -0500

    unión de header y footer en index

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function openSidebar() {
     document.getElementById("overlay").style.display = "none";
   }
   
+
+  fetch('/components/header.html')
+  .then(res => res.text())
+  .then(data => {
+    const header = document.getElementById('header');
+    if (header) header.innerHTML = data;
+  });
+
+fetch('/components/footer.html')
+  .then(res => res.text())
+  .then(data => {
+    const footer = document.getElementById('footer');
+    if (footer) footer.innerHTML = data;
+  });
